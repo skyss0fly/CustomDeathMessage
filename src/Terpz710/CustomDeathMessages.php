878ff837b846ecc63520190e67976bd26fa6cba8
Terpz710 <142659461+Terpz710@users.noreply.github.com>
2023-08-23 01:13:28 -0700

    Add files via upload

---
+++ src/Terpz710/CustomDeathMessages.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Terpz710;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerDeathEvent;
+use pocketmine\event\entity\EntityDamageByEntityEvent;
+use pocketmine\entity\Monster;
+use pocketmine\Player;
+use pocketmine\entity\Projectile;
+
+class CustomDeathMessages extends PluginBase implements Listener {
+
+    public function onEnable(): void {
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        $this->saveDefaultConfig();
+    }
+
+    public function onPlayerDeath(PlayerDeathEvent $event) {
+        $player = $event->getPlayer();
+        $cause = $player->getLastDamageCause();
+
+        if ($cause instanceof EntityDamageByEntityEvent) {
+            $killer = $cause->getDamager();
+            if ($killer instanceof Player) {
+                
+                $deathMessage = str_replace("{victim}", $player->getName(), $this->getConfig()->get("player_killed_by_player_message"));
+                $deathMessage = str_replace("{killer}", $killer->getName(), $deathMessage);
+            } else if ($killer instanceof Projectile && $killer->getOwningEntity() instanceof Player) {
+                
+                $owner = $killer->getOwningEntity();
+                $deathMessage = str_replace("{victim}", $player->getName(), $this->getConfig()->get("player_shot_by_projectile_message"));
+                $deathMessage = str_replace("{shooter}", $owner->getName(), $deathMessage);
+            } else if ($killer instanceof Monster) {
+                
+                $deathMessage = str_replace("{victim}", $player->getName(), $this->getConfig()->get("player_killed_by_mob_message"));
+                $deathMessage = str_replace("{mob}", $killer->getName(), $deathMessage);
+            }
+        }
+
+        if (!isset($deathMessage)) {
+            
+            $deathMessage = str_replace("{victim}", $player->getName(), $this->getConfig()->get("player_died_message"));
+        }
+
+        $event->setDeathMessage($deathMessage);
+    }
+
+}
